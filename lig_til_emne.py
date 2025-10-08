061183f146f4add5c729fd826619ba8c6ac74423
Polina-Fedorets <pofed8320@uis.no>
2025-10-08 15:16:40 +0200

    fix 4.

--- lig_til_emne.py
+++ lig_til_emne.py
@@ -8,26 +8,26 @@ def leg_til_emne_i_stidueplan(emne_index, semester):
     for sem in studieplan:
         if emne_index in sem:
             print(f'Feil: emne {emnekoder[emne_index]} er allerede i studieplanen.')
-            return False
+            return True
         
     if emne_tid == 'høst' and semester not in [1,3,5]:
             print(f'Feil: høstemne {emnekoder[emne_index]} kan bare legges til i 1, 3 og 5 semester.')
-            return False
+            return True
     elif emne_tid == 'vår' and semester not in [2,4,6]:
             print(f'Feil: våremne {emnekoder[emne_index]} kan legges bare i 2, 4 og 6. semester.')
-            return False
+            return True
         
 
 
     total_studiepoeng = sum(studiepoenger[i] for i in studieplan[semester_index])
     if total_studiepoeng + int(studiepoenger[emne_index]) > 30:
         print(f'Feil: for mye studiepoeg i semester {semester}, total studiepoeg er {total_studiepoeng}/30 poeg.')
-        return False
+        return True
         
 
     studieplan[semester_index].append(emne_index)
     print(f'Emne {emnekoder[emne_index]} lagt til i semester {semester}.')
-    return True
+    return False
 
 
 
